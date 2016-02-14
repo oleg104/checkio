53da47ba57a1858a36b14bf3396c1bfb7c6f1f8a
Oleg Abdullin <oleg.abdullin@globallogic.com>
2016-02-14 20:36:27 +0200

    resolved count_neighbours

---
+++ count_neighbours.py
@@ -0,0 +1,30 @@
+def count_neighbours(grid, row, col):
+    count_rows = len(grid)
+    count_columns = len(grid[0])
+
+    rows = [i for i in range(row - 1, row + 2) if i  >= 0 and i < count_rows]
+    columns = [i for i in range(col - 1, col + 2) if i  >= 0 and i < count_columns]
+
+    s = sum([grid[r][c] for r in rows for c in columns])
+
+    return s - grid[row][col]
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
